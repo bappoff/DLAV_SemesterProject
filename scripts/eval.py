b15536553448ecdc7fadd85543bc3d7ec77a23f8
bappoff <93776964+bappoff@users.noreply.github.com>
2023-05-13 15:55:31 +0200

    Update eval.py

--- scripts/eval.py
+++ scripts/eval.py
@@ -11,7 +11,7 @@ import torch
 import warnings
 from mmengine import Config, DictAction
 from mmcv.cnn import fuse_conv_bn
-from mmengine.parallel import MMDataParallel, MMDistributedDataParallel
+from mmengine.model import MMDataParallel, MMDistributedDataParallel
 from mmcv.runner import (get_dist_info, init_dist, load_checkpoint,
                          wrap_fp16_model)
 
