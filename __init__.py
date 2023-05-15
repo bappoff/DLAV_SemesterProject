5f0c7d39a055b8515e411638701c9bd99f0edd27
bappoff <93776964+bappoff@users.noreply.github.com>
2023-05-15 14:16:59 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,22 +1,22 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import mmcv
-import mmdet
-import mmengine
-from mmengine.utils import digit_version
+#import mmcv
+#import mmdet
+#import mmengine
+#from mmengine.utils import digit_version
 
-from .version import __version__, version_info
+#from .version import __version__, version_info
 
-mmcv_minimum_version = '2.0.0rc4'
-mmcv_maximum_version = '2.1.0'
-mmcv_version = digit_version(mmcv.__version__)
+#mmcv_minimum_version = '2.0.0rc4'
+#mmcv_maximum_version = '2.1.0'
+#mmcv_version = digit_version(mmcv.__version__)
 
-mmengine_minimum_version = '0.7.1'
-mmengine_maximum_version = '1.0.0'
-mmengine_version = digit_version(mmengine.__version__)
+#mmengine_minimum_version = '0.7.1'
+#mmengine_maximum_version = '1.0.0'
+#mmengine_version = digit_version(mmengine.__version__)
 
-mmdet_minimum_version = '3.0.0'
-mmdet_maximum_version = '3.1.0'
-mmdet_version = digit_version(mmdet.__version__)
+#mmdet_minimum_version = '3.0.0'
+#mmdet_maximum_version = '3.1.0'
+#mmdet_version = digit_version(mmdet.__version__)
 
 #assert (mmcv_version >= digit_version(mmcv_minimum_version)
 #        and mmcv_version < digit_version(mmcv_maximum_version)), \
