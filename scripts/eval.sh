1c68635f6bebc75ed1c7365542b71d2bfb760688
bappoff <93776964+bappoff@users.noreply.github.com>
2023-05-17 12:44:30 +0200

    Update eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -15,4 +15,4 @@ PORT=${PORT:-29503}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/tools/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4} --eval bbox
+    /work/scitas-share/voxformer/VoxFormer/tools/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4} --eval bbox
