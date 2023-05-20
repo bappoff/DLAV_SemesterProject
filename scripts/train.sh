23306c9afe40d341223a1046cfe8fe5f3db9a5fb
bappoff <93776964+bappoff@users.noreply.github.com>
2023-05-20 12:26:28 +0200

    Create train.sh

---
+++ scripts/train.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 20
+#SBATCH --begin=now
+#SBATCH --mem 40G
+#SBATCH --partition gpu
+#SBATCH --gres gpu:1
+
+CONFIG=$1
+GPUS=$2
+PORT=${PORT:-28509}
+
+PYTHONPATH="/home/bpoffet/miniconda3/envs/voxformer_venv/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    /work/scitas-share/voxformer/VoxFormer/tools/train.py $CONFIG --launcher pytorch ${@:3} --deterministic
